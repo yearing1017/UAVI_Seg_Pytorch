e34620ef64071131fa0185887552cf4dd86e871b
yearing1017 <33566006+yearing1017@users.noreply.github.com>
2020-02-11 14:00:02 +0800

    BGR转为RGB->进行标准化操作

--- MyData.py
+++ MyData.py
@@ -23,6 +23,7 @@ class MyDataset(Dataset):
     def __getitem__(self, idx):
         image_name = os.listdir('data/dataset1/images_prepped_train')[idx]
         image = cv2.imread('data/dataset1/images_prepped_train/' + image_name)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         # 读label,因为对应的label和原图对应，取一致的名字
         #mask_name = os.listdir('data/dataset1/annotations_prepped_train')[idx]
         mask = cv2.imread('data/dataset1/annotations_prepped_train/'+ image_name, 0)
