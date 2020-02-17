3be3716ec2f768cf1dc02de81f394b75ad33f577
yearing1017 <33566006+yearing1017@users.noreply.github.com>
2020-02-17 15:07:56 +0800

    更换PIL为cv2读取图像，并删除tensor方式读入

---
+++ MIoU/MIoUDatav0217.py
@@ -0,0 +1,46 @@
+import os 
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader, Dataset, random_split
+from torchvision import transforms
+from PIL import Image
+import cv2
+'''
+transform = transforms.Compose([
+    transforms.ToTensor()    
+])
+'''
+class MIoUDataset(Dataset):
+
+    def __init__(self, transform = None):
+        self.transform = transform
+
+    def __len__(self):
+        return len(os.listdir('data/dataset1/train_anno_noaug'))
+
+    def __getitem__(self, idx):
+        # 读label
+        label_name = os.listdir('data/dataset1/train_anno_noaug')[idx]
+        label = cv2.imread('data/dataset1/train_anno_noaug/' + label_name,0)
+        # 读 predict
+        predict = cv2.imread('predict_train_noaug_gray/' + label_name,0)
+        if self.transform:
+            label = self.transform(label)
+            predict = self.transform(predict)
+        return label, predict
+
+data = MIoUDataset()
+
+# 数据加载时会调用 __getitem__内置方法
+MIoU_dataloader = DataLoader(data, batch_size=1, shuffle=True, num_workers=1)
+
+if __name__ == "__main__":
+    for index, (label, predict) in enumerate(MIoU_dataloader):
+        print(index)
+        print('=============')
+        print(label)
+        print('=============')
+        print(predict)
+        print('=============')
+        if index > 1:
+            break
